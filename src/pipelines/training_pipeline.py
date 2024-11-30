dffb70cd750d078dca70629cae5d7ae7f53af13e
sourav <souravshukla985@gmail.com>
2024-11-30 15:44:39 +0530

    data transformation and training pipeline has been done

--- src/pipelines/training_pipeline.py
+++ src/pipelines/training_pipeline.py
@@ -1,13 +1,36 @@
-import os 
 import sys
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),"../../")))
-from src.logger import logging
-from src.exception import CustomException
-import pandas as pd 
 from src.components.data_ingestion import DataIngestion
+from src.components.data_transformation import DataTransformation
+from src.components.model_trainer import ModelTrainer
+from src.exception import CustomException
 
 
-if __name__=="__main__":
-    obj=DataIngestion()
-    train_data_path,test_data_path=obj.initiate_data_ingestion()
-    print(train_data_path,test_data_path)
+class TrainPipeline:
+    def __init__(self) -> None:
+        self.data_ingestion = DataIngestion()
+
+        self.data_transformation = DataTransformation()
+
+        self.model_trainer = ModelTrainer()
+
+    def run_pipeline(self):
+        try:
+            train_path, test_path = self.data_ingestion.initiate_data_ingestion()
+
+            (
+                train_arr,
+                test_arr,
+                preprocessor_file_path,
+            ) = self.data_transformation.initiate_data_transformation(
+                train_path=train_path, test_path=test_path
+            )
+
+            r2_square = self.model_trainer.initiate_model_trainer(
+                train_array=train_arr,
+                test_array=test_arr,
+                preprocessor_path=preprocessor_file_path,
+            )
+            print("training completed. Trained model score : ", r2_square)
+
+        except Exception as e:
+            raise CustomException(e, sys)
--- src/utils.py
+++ src/utils.py
@@ -39,3 +39,16 @@ def export_collection_as_dataframe(collection_name, db_name):
 
     except Exception as e:
         raise CustomException(e, sys)
+
+
+def save_object(file_path, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open(file_path, "wb") as file_obj:
+            dill.dump(obj, file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
