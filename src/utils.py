d55c648b0945c38c367fab60dcd22b47f5e97ab4
sourav <souravshukla985@gmail.com>
2024-12-02 00:13:32 +0530

    this is deployment commit

--- src/utils.py
+++ src/utils.py
@@ -52,3 +52,66 @@ def save_object(file_path, obj):
 
     except Exception as e:
         raise CustomException(e, sys)
+
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
+
+
+def upload_file(from_filename, to_filename, bucket_name):
+    try:
+        s3_resource = boto3.resource("s3")
+
+        s3_resource.meta.client.upload_file(from_filename, bucket_name, to_filename)
+
+    except Exception as e:
+        raise CustomException(e, sys)
+
+
+def download_model(bucket_name, bucket_file_name, dest_file_name):
+    try:
+        s3_client = boto3.client("s3")
+
+        s3_client.download_file(bucket_name, bucket_file_name, dest_file_name)
+
+        return dest_file_name
+
+    except Exception as e:
+        raise CustomException(e, sys)
+
+
+
+
+
+def evaluate_models(X, y, models):
+    try:
+        X_train, X_test, y_train, y_test = train_test_split(
+            X, y, test_size=0.2, random_state=42
+        )
+
+        report = {}
+
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+
+            model.fit(X_train, y_train)  # Train model
+
+            y_train_pred = model.predict(X_train)
+
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
